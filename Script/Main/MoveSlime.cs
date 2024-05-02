46833459e90660e11df24927f326c6e8f38d1293
hhcczz <101077489+hhcczz@users.noreply.github.com>
2024-05-03 04:58:17 +0900

    메인 - 큐티
    회전 추가했음

--- Script/Main/MoveSlime.cs
+++ Script/Main/MoveSlime.cs
@@ -11,57 +11,62 @@ public class MoveSlime : MonoBehaviour
 
     public GameObject[] Slime;
 
-    private void Awake()
-    {
-        enemyManager = EnemyManager.Instance;
-    }
 
     private void Start()
     {
+        enemyManager = EnemyManager.Instance;
         CuteSlimeChange();
     }
 
     public void CuteSlimeChange()
     {
-
-        // RectTransform 컴포넌트 가져오기
-        RectTransform newslime = Slime[enemyManager.FixDifficulty].GetComponent<RectTransform>();
+        int slimeIndex = enemyManager.FixDifficulty + enemyManager.FixDifficultyInStage * 4;
+        GameObject newslime = Slime[slimeIndex];
+        RectTransform newslimeTransform = newslime.GetComponent<RectTransform>();
 
         for (int i = 0; i < Slime.Length; i++)
         {
-            if (i == enemyManager.FixDifficulty)
-            {
-                Slime[i].SetActive(true);
-            }
-            else
-            {
-                Slime[i].SetActive(false);
-            }
+            Slime[i].SetActive(i == slimeIndex);
         }
 
         // 슬라임을 이동 방향으로 이동
         if (movingForward)
         {
-            newslime.Translate(speed * Time.deltaTime * Vector2.right);
+            if (enemyManager.FixDifficultyInStage == 1) newslimeTransform.Translate(speed * Time.deltaTime * Vector2.left);
+            else newslimeTransform.Translate(speed * Time.deltaTime * Vector2.right);
 
             // 최대 이동 거리에 도달하면 이동 방향 변경
-            if (newslime.anchoredPosition.x >= maxDistance)
+            if (newslimeTransform.anchoredPosition.x >= maxDistance)
             {
                 movingForward = false;
+                if (enemyManager.FixDifficultyInStage == 1)
+                {
+                    // 슬라임의 방향을 변경합니다.
+                    newslimeTransform.localRotation = Quaternion.Euler(0, 0, 0);
+                }
+                else newslimeTransform.localRotation = Quaternion.Euler(0, 180, 0);
             }
         }
         else
         {
             // 초기 위치로 되돌아가는 이동
-            newslime.Translate(speed * Time.deltaTime * Vector2.left);
+            if (enemyManager.FixDifficultyInStage == 1) newslimeTransform.Translate(speed * Time.deltaTime * Vector2.left);
+            else newslimeTransform.Translate(speed * Time.deltaTime * Vector2.right);
 
             // 초기 위치에 도달하면 다시 이동 방향을 전진으로 변경
-            if (newslime.anchoredPosition.x <= 0f)
+            if (newslimeTransform.anchoredPosition.x <= 0f)
             {
                 movingForward = true;
+                if (enemyManager.FixDifficultyInStage == 1)
+                {
+                    // 슬라임의 방향을 변경합니다.
+                    newslimeTransform.localRotation = Quaternion.Euler(0, 180, 0);
+                }
+                else newslimeTransform.localRotation = Quaternion.Euler(0, 0, 0);
             }
         }
     }
+
 
     private void Update()
     {
