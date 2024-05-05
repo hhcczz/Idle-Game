d146491d8a5b81e67e0bac9c33d3a81ee3b5ec86
hhcczz <101077489+hhcczz@users.noreply.github.com>
2024-05-06 00:56:28 +0900

    시스템 - 돈 관리
    만들었음

---
+++ Script/System/GoldManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldManager : MonoBehaviour
+{
+
+
+    [System.Obsolete]
+    public static long Gold(int MonsterValue)
+    {
+        int Warrant_Random = Random.Range(0, 100);  // 경험치 N배 확률
+
+        long gold = (long)(MonsterValue * (1 + ((float)GameManager.Player_MoneyUp_EarnMoney + MobScrollManager.MS_UpEarnGold) / 100f));
+
+        if (GameManager.WarrantLevel[20] >= 1) gold *= (long)(1 + (float)GameManager.Warrant_Power[12] / 100f);
+
+        if (AdManager.AdPlaying[1] == true) gold *= (long)((AdManager.AdPowerValue[1] + 100) / 100f);
+
+        if (GameManager.WarrantLevel[11] >= 1 && Warrant_Random < 8) gold *= (int)(1 + (float)GameManager.Warrant_Power[11] / 100);
+
+        return gold;
+    }
+}
