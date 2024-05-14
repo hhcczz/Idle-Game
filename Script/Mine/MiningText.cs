53fd619ebab38ba93df423ebd88d2bf7d2ce6057
hhcczz <101077489+hhcczz@users.noreply.github.com>
2024-05-15 02:51:19 +0900

    광산 - 텍스트
    수정했음

--- Script/Mine/MiningText.cs
+++ Script/Mine/MiningText.cs
@@ -4,36 +4,35 @@ using UnityEngine.UI;
 /*
  * File :   MiningText.cs
  * Desc :   광산 데미지 표시 하는거 위로 올리기
- *         
+ *
  *
  & Functions
  &  [Public]
- &  : 
- &  : 
+ &  :
+ &  :
  &
  &  [Protected]
  &  : DestroyObject() - 시간되면 부수기
- &  : 
- &  : 
- &  : 
+ &  :
+ &  :
+ &  :
  &
  &  [Private]
- &  : 
- *
+ &
  */
 public class MiningText : MonoBehaviour
 {
-    public float moveSpeed;     // 텍스트 이동속도
-    public float AlphaSpeed;    // 투명도 변화속도
-    public float destroyTime;
+    public float moveSpeed = 1.1f;     // 텍스트 이동속도
+    public float alphaSpeed = 3.5f;    // 투명도 변화속도
+    public float destroyTime = 1.2f;   // 텍스트가 사라지기 전 대기 시간
 
-    private Color Alpha;
+    private Text textComponent;
+    private Color alphaColor;
 
     private void Start()
     {
-        moveSpeed = 1.5f;
-        AlphaSpeed = 0.4f;
-        Alpha = GetComponent<Text>().color; // 알파값을 초기화합니다.
+        textComponent = GetComponent<Text>();
+        alphaColor = textComponent.color;
 
         Invoke(nameof(DestroyObject), destroyTime);
     }
@@ -42,10 +41,10 @@ public class MiningText : MonoBehaviour
     {
         transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
 
-        if (Alpha.a > 50f / 255f)  // 알파값이 N보다 크다면 계속해서 진행
+        if (alphaColor.a > 50f / 255f)  // 알파값이 50/255보다 크다면 계속해서 진행
         {
-            Alpha.a = Mathf.Lerp(Alpha.a, 0, Time.deltaTime * AlphaSpeed);  // 투명도 조절
-            GetComponent<Text>().color = Alpha; // 변경된 알파값을 설정합니다.
+            alphaColor.a = Mathf.Lerp(alphaColor.a, 0, Time.deltaTime * alphaSpeed);  // 투명도 조절
+            textComponent.color = alphaColor; // 변경된 알파값을 설정합니다.
         }
     }
 
